c0a78e8a1d9400b303bcbe6a98531a4edf864b8c
DIegoCordeiro14 <57020531+DIegoCordeiro14@users.noreply.github.com>
2026-06-06 00:01:31 -0300

    Add files via upload

---
+++ src/app/app/atualizacoes/page.tsx
@@ -0,0 +1,7 @@
+import { UpdateManager } from '@/components/edn/update-manager';
+
+export const dynamic = 'force-static';
+
+export default function AtualizacoesPage() {
+  return <UpdateManager />;
+}
